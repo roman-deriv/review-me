7704200e67a4f7935c8a608d682c223bed8fdec4
BenGrif2323 <83267311+BenGrif2323@users.noreply.github.com>
2024-07-02 10:46:51 -0700

    refined overview template

--- src/ai/templates.py
+++ src/ai/templates.py
@@ -5,18 +5,27 @@ def load():
     loaded = dict(
         preamble = env.get_template('preamble.txt'),
         overview = env.get_template('overview.txt')
+
     )
     return loaded
 
 templates = load()
-print(templates['preamble'].render( 
-                            title="Test Title",
-                            description="Test Description",
-                            commit_messages=["Test", "Commit", "Messages"]
-                          ))
 
-print(templates['overview'].render(
+def preamble():
+    return templates['preamble'].render( 
+                            title = "Test Title",
+                            description = "Test Description",
+                            commit_messages = ["Test", "Commit", "Messages"]
+                          )
+
+def overview():
+    return preamble() + templates['overview'].render(
+                            added_files = "added files (test)",
+                            deleted_files = "deleted files (test)",
+                            modified_files = "modified files (test)",
                             diffs={
                                 "filename1":"diff1",
                                 "filename2":"diff2"
-                                }))
+                                })
+
+print(overview())
