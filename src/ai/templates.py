8e862e4a0d593d7fb4d54c83d18c8e3ce02c1332
BenGrif2323 <83267311+BenGrif2323@users.noreply.github.com>
2024-07-02 10:25:49 -0700

    created overview template

--- src/ai/templates.py
+++ src/ai/templates.py
@@ -3,13 +3,20 @@ from jinja2 import Environment, PackageLoader
 def load():
     env = Environment(loader=PackageLoader('templates'))
     loaded = dict(
-        preamble = env.get_template('preamble.txt')
+        preamble = env.get_template('preamble.txt'),
+        overview = env.get_template('overview.txt')
     )
     return loaded
 
 templates = load()
 print(templates['preamble'].render( 
-                          title="Test Title",
-                          description="Test Description",
-                          commit_messages=["Test", "Commit", "Messages"]
+                            title="Test Title",
+                            description="Test Description",
+                            commit_messages=["Test", "Commit", "Messages"]
                           ))
+
+print(templates['overview'].render(
+                            diffs={
+                                "filename1":"diff1",
+                                "filename2":"diff2"
+                                }))
