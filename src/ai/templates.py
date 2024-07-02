dc17ea744495b0ffd571b7f4c9c4c75839293ef2
BenGrif2323 <83267311+BenGrif2323@users.noreply.github.com>
2024-07-02 09:50:03 -0700

    Created Preamble Template

--- src/ai/templates.py
+++ src/ai/templates.py
@@ -3,10 +3,13 @@ from jinja2 import Environment, PackageLoader
 def load_templates():
     env = Environment(loader=PackageLoader('templates'))
     loaded = []
-    loaded.append(env.get_template('test.txt'))
+    loaded.append(env.get_template('Preamble.txt'))
     loaded.append(env.get_template('test2.txt'))
     return loaded
 
 templates = load_templates()
-print(templates[0].render(testvar="test#1"))
-print(templates[1].render(testvar="test#2"))
+print(templates[0].render( 
+                          title="Test Title",
+                          description="Test Description",
+                          commit_messages=["Test", "Commit", "Messages"]
+                          ))
