ebb1268cf04e4f6541b73b6c9fa0282559584844
BenGrif2323 <83267311+BenGrif2323@users.noreply.github.com>
2024-07-04 10:39:14 -0700

    added errors (and try-excepts)

--- src/main.py
+++ src/main.py
@@ -1,22 +1,38 @@
 import asyncio
+import traceback
 
 import ai.assistant
 import ai.prompt
 import config
 from app import App, get_pr, build_context
+import logger
 
 
 def main():
-    cfg = config.from_env()
-    pr = get_pr(cfg.github)
 
-    context = build_context(pr)
-    builder = ai.prompt.Builder(context)
-    assistant = ai.assistant.Assistant(cfg.llm.model, builder)
 
-    app = App(pr, assistant, debug=cfg.debug)
+    try:
+        cfg = config.from_env()
+        pr = get_pr(cfg.github)
+    except Exception as e:
+        logger.log.debug(f"Problem during inital setup: {e}")
+        exit(69)
+    
+    try:
+        assert False
+        context = build_context(pr)
+        builder = ai.prompt.Builder(context)
+        assistant = ai.assistant.Assistant(cfg.llm.model, builder)
 
-    asyncio.run(app.run())
+        app = App(pr, assistant, debug=cfg.debug)
+
+        asyncio.run(app.run())
+    except Exception as e:
+        logger.log.debug(f"Problem during run: {e}")
+        pr.create_issue_comment(f"{traceback.format_exc()}")
+        exit(42)
+
+    
 
 
 if __name__ == "__main__":
