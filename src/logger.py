b48dcb70b81655f269db589abf1551732f5533e9
FyZyX <lucasmlofaro@gmail.com>
2024-07-04 15:23:00 -0700

    improve logging

--- src/logger.py
+++ src/logger.py
@@ -1,9 +1,33 @@
 import logging
 import os
 
+LOG_FILE = os.environ.get("LOGFILE", "default_logfile.log")
 
-logging.basicConfig(level=logging.DEBUG, 
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    handlers=[logging.FileHandler(os.environ.get("LOGFILE","default_logfile.log")), logging.StreamHandler()])
 
-log = logging.getLogger(__name__)
+def _init_logger():
+    log = logging.getLogger(__name__)
+    log.setLevel(logging.DEBUG)
+
+    file_handler = logging.FileHandler(LOG_FILE)
+    file_handler.setLevel(logging.INFO)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+    console_handler.setFormatter(formatter)
+
+    log.addHandler(file_handler)
+    log.addHandler(console_handler)
+
+    return log
+
+
+class LogFilter(logging.Filter):
+    def filter(self, record):
+        return record.name.startswith(__name__)
+
+
+log = _init_logger()
