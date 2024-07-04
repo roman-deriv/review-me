0a79c8a94b193e1b27331d0e071994bb76239f52
BenGrif2323 <83267311+BenGrif2323@users.noreply.github.com>
2024-07-04 11:04:12 -0700

    imporved log messages and try-except implementation

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 import asyncio
+import sys
 import traceback
+
+import github
 
 import ai.assistant
 import ai.prompt
@@ -14,12 +17,14 @@ def main():
     try:
         cfg = config.from_env()
         pr = get_pr(cfg.github)
+    except github.GithubException as e:
+        logger.log.critical(f"Couldn't retrieve pull request from Github: {e}")
+        sys.exit(69)
     except Exception as e:
-        logger.log.debug(f"Problem during inital setup: {e}")
-        exit(69)
+        logger.log.critical(f"Problem during inital setup: {e}")
+        sys.exit(42)
     
     try:
-        assert False
         context = build_context(pr)
         builder = ai.prompt.Builder(context)
         assistant = ai.assistant.Assistant(cfg.llm.model, builder)
@@ -28,9 +33,12 @@ def main():
 
         asyncio.run(app.run())
     except Exception as e:
-        logger.log.debug(f"Problem during run: {e}")
-        pr.create_issue_comment(f"Sorry, couldn't review your code becasue\n```{traceback.format_exc()}```")
-        exit(42)
+        logger.log.error(f"Problem during run: {e}")
+        try:
+            pr.create_issue_comment(f"Sorry, couldn't review your code becasue\n```{traceback.format_exc()}```")
+        except github.GithubException as e2:
+            logger.log.error(f"Problem posting error comment: {e2}")
+            sys.exit(69)
 
     
 
