d94693aed9e1eac17d4fc24e8f3aecee960b650a
FyZyX <lucasmlofaro@gmail.com>
2024-07-04 23:36:54 -0700

    create hunk model

--- src/model.py
+++ src/model.py
@@ -33,6 +33,8 @@ class FileReviewRequest:
     changes: str
     related_changed: str
     reason: str
+    diff: str
+
 
 @dataclasses.dataclass
 class ReviewContext:
@@ -45,6 +47,41 @@ class ReviewContext:
     added_files: list[Filename]
     modified_files: list[Filename]
     deleted_files: list[Filename]
+
+
+@dataclasses.dataclass
+class Hunk:
+    start_line: int
+    end_line: int
+    changed_lines: set[int]
+
+    def contains(self, line: int) -> bool:
+        return self.start_line <= line <= self.end_line
+
+    def is_changed_line(self, line: int) -> bool:
+        return line in self.changed_lines
+
+    def contains_comment(self, comment: Comment) -> bool:
+        if "end_line" in comment:
+            comment_line = int(comment["end_line"])
+            if self.contains(comment_line):
+                return self.is_changed_line(comment_line)
+        elif "start_line" in comment and "end_line" in comment:
+            comment_start = int(comment["start_line"])
+            comment_end = int(comment["end_line"])
+
+            is_comment_in_hunk = (
+                    self.contains(comment_start) and
+                    self.contains(comment_end)
+            )
+
+            if is_comment_in_hunk:
+                return any(
+                    line in self.changed_lines
+                    for line in range(comment_start, comment_end + 1)
+                )
+
+        return False
 
 
 @dataclasses.dataclass
