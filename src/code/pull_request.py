e9a605bf9ffb4f05214a1960b2f62826612f30f9
BenGrif2323 <83267311+BenGrif2323@users.noreply.github.com>
2024-07-05 21:48:41 -0700

    moved posting comments to the pr to pull_request.py, moved some error handling there for consistency

--- src/code/pull_request.py
+++ src/code/pull_request.py
@@ -1,10 +1,22 @@
 import github
+import sys
 
 import config
+import logger
 
 
 def get_pr(cfg: config.GitHubConfig):
-    gh = github.Github(cfg.token)
-    repo = gh.get_repo(cfg.repository)
-    pr = repo.get_pull(cfg.pr_number)
-    return pr
+    try:
+        gh = github.Github(cfg.token)
+        repo = gh.get_repo(cfg.repository)
+        pr = repo.get_pull(cfg.pr_number)
+        return pr
+    except Exception as e:
+        logger.log.critical(f"Couldn't retrieve pull request from Github: {e}")
+        sys.exit(69)
+
+def comment(pr, message):
+    try:
+        pr.create_issue_comment(message)
+    except Exception as e:
+        logger.log.error(f"Problem posting comment: {e}")
