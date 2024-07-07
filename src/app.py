6facc1c8cf4d5e53f1a8b892054e48d8ea60e0d9
FyZyX <lucasmlofaro@gmail.com>
2024-07-06 17:57:15 -0700

    update data model imports

--- src/app.py
+++ src/app.py
@@ -4,16 +4,17 @@ from github.PullRequest import PullRequest
 
 import ai.assistant
 import ai.tool
+import code.model
 import code.pull_request
+import code.review.model
 import logger
-import model
 
 
 class App:
     def __init__(
             self,
             pull_request: PullRequest,
-            context: model.ReviewContext,
+            context: code.review.model.PullRequestContextModel,
             assistant: ai.assistant.Assistant,
             debug: bool = False,
     ):
@@ -24,41 +25,41 @@ class App:
 
     async def _review_file(
             self,
-            review_request: model.FileReviewRequest,
+            context: code.review.model.FileContextModel,
             delay: float,
-    ) -> list[model.Comment]:
-        if not review_request.diff:
+    ) -> list[code.model.GitHubCommentModel]:
+        if not context.patch.diff:
             return []
 
         # Stagger request start times to comply with rate limits
         logger.log.debug(f"Waiting {delay} seconds before reviewing")
         await asyncio.sleep(delay)
 
-        file_comments = await self._assistant.review_file(review_request)
+        file_comments = await self._assistant.review_file(context)
 
         return file_comments
 
     async def _review_files(
             self,
-            review_requests: list[model.FileReviewRequest],
-    ) -> list[model.Comment]:
+            contexts: list[code.review.model.FileContextModel],
+    ) -> list[code.model.GitHubCommentModel]:
         return [
             comment
             for file_comments in await asyncio.gather(*[
-                asyncio.create_task(self._review_file(req, delay=i * 3.5))
-                for i, req in enumerate(review_requests)
+                asyncio.create_task(self._review_file(context, delay=i * 3.5))
+                for i, context in enumerate(contexts)
             ])
             for comment in file_comments
         ]
 
     async def run(self):
-        review_requests = await self._assistant.request_reviews(self._context)
+        file_contexts = await self._assistant.request_reviews(self._context)
         logger.log.debug(
             f"Files to review: \n"
-            f"- {"\n- ".join([r.path for r in review_requests])}"
+            f"- {"\n- ".join([context.path for context in file_contexts])}"
         )
 
-        comments = await self._review_files(review_requests)
+        comments = await self._review_files(file_contexts)
 
         feedback = await self._assistant.get_feedback(comments)
         logger.log.info(f"Overall Feedback: {feedback.evaluation}")
