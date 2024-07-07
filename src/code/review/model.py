fb4613839fe83b4676417ddc862d0f998a090f4c
FyZyX <lucasmlofaro@gmail.com>
2024-07-06 23:54:33 -0700

    add overview data model

--- src/code/review/model.py
+++ src/code/review/model.py
@@ -1,3 +1,5 @@
+import enum
+
 from pydantic import BaseModel
 
 from ..model import FilePatchModel, GitHubCommentModel
@@ -21,6 +23,35 @@ class FileContextModel(BaseModel):
     related_changes: str
     reason: str
     patch: FilePatchModel
+
+
+class Status(enum.IntEnum):
+    ACCEPTABLE = 0
+    REVIEW_REQUIRED = 1
+    UNACCEPTABLE = 2
+
+
+class InitialAssessmentModel(BaseModel):
+    status: Status
+    summary: str
+
+
+class ObservationTag(enum.StrEnum):
+    DOCUMENTATION = "documentation"
+    CODE_STYLE = "code-style"
+    TYPE_CHECKING = "type-checking"
+    ERROR_HANDLING = "error-handling"
+
+
+class ObservationModel(BaseModel):
+    comment: str
+    tag: ObservationTag
+
+
+class OverviewModel(BaseModel):
+    initial_assessment: InitialAssessmentModel
+    observations: list[ObservationModel]
+    file_contexts: list[FileContextModel]
 
 
 class Feedback(BaseModel):
